40841c8916daac3ee59cc02e9aeef9b93723701b
chennan6 <32255543+chennan6@users.noreply.github.com>
2018-05-30 16:16:51 +0800

    Add files via upload

---
+++ baidu/16/index.js
@@ -0,0 +1,8 @@
+function lookmore() {
+    var t=document.getElementById("viewmore");
+    t.innerHTML=("<h1>Hello World</h1>");
+}
+function looktime() {
+    var t=document.getElementById("viewtime");
+    t.innerHTML=Date();
+}
